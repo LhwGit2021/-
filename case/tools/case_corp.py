3896bdd45c92cb5c385f794fc341b3326d3e065f
LhwGit2021 <91765967+LhwGit2021@users.noreply.github.com>
2021-10-07 09:28:44 +0800

    Update case_corp.py

--- case/tools/case_corp.py
+++ case/tools/case_corp.py
@@ -3,11 +3,11 @@ from jsonpath import jsonpath
 from WeWork.api.api_corp import QY_interface
 
 
-# def getId():
-#     inter = QY_interface()
-#     r = inter.tag_select()
-#     id = jsonpath(r.json(), "$..[?(@.name=='无与伦比')]")[0]['id']
-#     return id
+def getId():
+    inter = QY_interface()
+    r = inter.tag_select()
+    id = jsonpath(r.json(), "$..[?(@.name=='无与伦比')]")[0]['id']
+    return id
 
 def get_Id():
     inter = QY_interface()
@@ -29,25 +29,25 @@ class TestCorpTag:
     def test_demo(self):
         pass
 
-    #
-    #
-    # def test_02add(self):
-    #     data = {
-    #         "group_id": "et3OWZBwAAh8z8yGjJ34cs9IifhxffKQ",
-    #         "group_name": "客户等级"
-    #     }
-    #     r = self.inter.tag_add("无与伦比", **data)
-    #
-    # @pytest.mark.parametrize('id,name,check', [
-    #     [getId(), "蛤蟆", 0],
-    #     [getId(), "老鼠", 0],
-    #     [getId(), "蝎子", 0],
-    #     [getId(), "蜘蛛", 0],
-    #     [getId(), "🐍", 0],
-    # ], ids=["01", "02", "03", "04", "05"])
-    # def test_03update(self, id, name, check):
-    #     r = self.inter.tag_update(id, name)
-    #     assert r.json()["errcode"] == check
+    
+    
+    def test_02add(self):
+        data = {
+            "group_id": "et3OWZBwAAh8z8yGjJ34cs9IifhxffKQ",
+            "group_name": "客户等级"
+        }
+        r = self.inter.tag_add("无与伦比", **data)
+    
+    @pytest.mark.parametrize('id,name,check', [
+        [getId(), "蛤蟆", 0],
+        [getId(), "老鼠", 0],
+        [getId(), "蝎子", 0],
+        [getId(), "蜘蛛", 0],
+        [getId(), "蛤蟆", 0],
+    ], ids=["01", "02", "03", "04", "05"])
+    def test_03update(self, id, name, check):
+        r = self.inter.tag_update(id, name)
+        assert r.json()["errcode"] == check
 
     def test_04delete(self):
         data = {
